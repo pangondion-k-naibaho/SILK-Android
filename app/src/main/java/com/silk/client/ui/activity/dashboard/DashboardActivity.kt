6156c258bbea81a322c370a1e6fe43f859c31e81
pangondion <pangondion.naibaho@vnt.co.id>
2024-10-31 12:14:32 +0700

    SILK : update slicing dashboard, integrating log out

--- app/src/main/java/com/silk/client/ui/activity/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/silk/client/ui/activity/dashboard/DashboardActivity.kt
@@ -4,11 +4,17 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import com.silk.client.R
+import com.silk.client.data.Constants.PREFERENCES.Companion.APP_PREFERENCES
+import com.silk.client.data.Constants.PREFERENCES.Companion.TOKEN_KEY
 import com.silk.client.databinding.ActivityDashboardBinding
+import com.silk.client.ui.activity.authentication.AuthenticationActivity
 
 class DashboardActivity : AppCompatActivity() {
     private val TAG = DashboardActivity::class.java.simpleName
@@ -29,6 +35,9 @@ class DashboardActivity : AppCompatActivity() {
     private fun setUpView(){
         binding.dashboardToolbar.navigationIcon = ContextCompat.getDrawable(this@DashboardActivity, R.drawable.ic_burger_menu)
 
+        setSupportActionBar(binding.dashboardToolbar)
+
+
         val toggle = ActionBarDrawerToggle(
             this@DashboardActivity, binding.drawerDashboard, binding.dashboardToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
         )
@@ -42,6 +51,40 @@ class DashboardActivity : AppCompatActivity() {
             } else {
                 binding.drawerDashboard.openDrawer(GravityCompat.START)
             }
+        }
+
+        binding.btnLogout.setOnClickListener {
+            val appPreferences = this@DashboardActivity.getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE)
+
+            val editor = appPreferences.edit()
+
+            editor.remove(TOKEN_KEY)
+
+            if(editor.commit()){
+                Toast.makeText(this@DashboardActivity, getString(R.string.toastLogoutSuccess), Toast.LENGTH_SHORT).show()
+                startActivity(AuthenticationActivity.newIntent(this@DashboardActivity))
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+                finish()
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.dashboard_toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.menuCart -> {
+                Toast.makeText(this@DashboardActivity, getString(R.string.toastUnderDev), Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.menuNotification -> {
+                Toast.makeText(this@DashboardActivity, getString(R.string.toastUnderDev), Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
